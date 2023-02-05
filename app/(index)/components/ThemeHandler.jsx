df1cd8e9f2e35a1c34b979a8b7e9fc5253ced72a
fabio alcocer <fabioalcocersejas17@gmail.com>
2023-02-05 15:12:16 -0400

    change theme switcher

--- app/(index)/components/ThemeHandler.jsx
+++ app/(index)/components/ThemeHandler.jsx
@@ -1,6 +1,5 @@
 'use client'
 import { useState, useEffect } from 'react'
-import { BsMoonStars, BsSun } from 'react-icons/bs'
 
 function ThemeHandler () {
   const [theme, setTheme] = useState('dark')
@@ -14,19 +13,23 @@ function ThemeHandler () {
   }
 
   return (
-    <button
-      className='group flex items-center rounded-full bg-white/90 px-3 py-[10px] text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/40 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20'
-      onClick={handleTheme}
-      name='theme switcher'
-    >
-      {theme === 'light'
-        ? (
-          <BsSun className='text-base' />
-          )
-        : (
-          <BsMoonStars className='text-base' />
-          )}
-    </button>
+    <label className='switch'>
+      <input type='checkbox' onClick={handleTheme} />
+      <span className='slider' />
+    </label>
+  // <button
+  //   className='group flex items-center rounded-full bg-white/90 px-3 py-[10px] text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/40 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20'
+  //   onClick={handleTheme}
+  //   name='theme switcher'
+  // >
+  //   {theme === 'light'
+  //     ? (
+  //       <BsSun className='text-base' />
+  //       )
+  //     : (
+  //       <BsMoonStars className='text-base' />
+  //       )}
+  // </button>
   )
 }
 
