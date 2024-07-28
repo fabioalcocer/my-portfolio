e914bfaac643527197ba54ec20cf33f5139b84a9
fabioalcocer <fabioalcocerdev@gmail.com>
2024-07-28 13:37:48 -0400

    add aurora bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
-import FeaturedProjects from '@/components/FeaturedProjects'
-import Contact from '@/components/Contact'
-import Hero from '@/components/Hero'
-import SkillsBoard from '@/components/SkillsBoard'
-import Experience from '@/components/Experience'
-import Container from '@/components/Container'
+import { AuroraBackground } from "@/components/ui/aurora-background"
+import Contact from "@/components/Contact"
+import Container from "@/components/Container"
+import Experience from "@/components/Experience"
+import FeaturedProjects from "@/components/FeaturedProjects"
+import Hero from "@/components/Hero"
+import SkillsBoard from "@/components/SkillsBoard"
 
-export default function Home () {
+export default function Home() {
   return (
     <Container>
-      <Hero />
+      <AuroraBackground>
+        <Hero />
+      </AuroraBackground>
+      
       <Experience />
       <FeaturedProjects />
       <SkillsBoard />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,75 @@
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
-    './app/**/*.{js,ts,jsx,tsx}', // Note the addition of the `app` directory.
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './src/**/*.{js,ts,jsx,tsx}'
+    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-conic': 'conic-gradient(var(--tw-gradient-stops))'
+        "gradient-conic": "conic-gradient(var(--tw-gradient-stops))",
       },
       animation: {
-        'bounce-slow': 'bounce 2s linear infinite',
-        disco: 'disco 2.5s linear infinite'
+        "bounce-slow": "bounce 2s linear infinite",
+        disco: "disco 2.5s linear infinite",
+        aurora: "aurora 60s linear infinite",
       },
       keyframes: {
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
         disco: {
-          '0%': { transform: 'translateY(-50%) rotate(0deg)' },
-          '100%': { transform: 'translateY(-50%) rotate(360deg)' }
+          "0%": { transform: "translateY(-50%) rotate(0deg)" },
+          "100%": { transform: "translateY(-50%) rotate(360deg)" },
         },
         btnpulse: {
-          '0%': {
-            'box-shadow': '0 0 0 0 theme("colors.zinc.400/80")'
+          "0%": {
+            "box-shadow": '0 0 0 0 theme("colors.zinc.400/80")',
           },
-          '30%': {
-            'box-shadow': '0 0 0 0 theme("colors.zinc.400/40")'
+          "30%": {
+            "box-shadow": '0 0 0 0 theme("colors.zinc.400/40")',
           },
-          '100%': {
-            'box-shadow': '0 0 0 12px theme("colors.zinc.400/0")'
-          }
+          "100%": {
+            "box-shadow": '0 0 0 12px theme("colors.zinc.400/0")',
+          },
         },
         rightGrow: {
-          '0%': {
-            width: '0'
+          "0%": {
+            width: "0",
           },
-          '100%': {
-            width: '100%'
-          }
-        }
-      }
-    }
+          "100%": {
+            width: "100%",
+          },
+        },
+      },
+    },
   },
-  plugins: [require('@headlessui/tailwindcss')({ prefix: 'ui' })]
+  plugins: [
+    require("@headlessui/tailwindcss")({ prefix: "ui" }),
+    addVariablesForColors,
+  ],
+}
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"))
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  )
+
+  addBase({
+    ":root": newVars,
+  })
 }
